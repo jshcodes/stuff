b9916bce37240a43ab95c072053b30ff41cf1aa1
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-11 16:24:17 -0500

    More stuff

---
+++ initialize_webserver_aws_ami.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo 'Starting'
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+echo $REGION
+echo 'Configuring region'
+aws configure set region $REGION
+
+#installing from current directory 
+sudo yum update -y
+sudo yum install -y python3-pip python3 python3-setuptools build-essential libssl-dev libffi-dev git
+sudo amazon-linux-extras install docker
+
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "version: '3'" > docker-compose.yml
+echo "services:" >> docker-compose.yml
+echo "  jenkins:" >> docker-compose.yml
+#echo "    image: franklinjff/jenkins:version1" >> docker-compose.yml
+echo "    image: jharris10/jenkins:2" >> docker-compose.yml
+echo "    environment:" >> docker-compose.yml
+echo "      JAVA_OPTS: \"-Djava.awt.headless=true\"" >> docker-compose.yml
+echo "      JAVA_OPTS: \"-Djenkins.install.runSetupWizard=false\"" >> docker-compose.yml
+echo "    ports:" >> docker-compose.yml
+echo "      - \"50000:50000\"" >> docker-compose.yml
+echo "      - \"8080:8080\"" >> docker-compose.yml
+sudo docker-compose up -d
