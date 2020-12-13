11255a2c017df10cc91ec3921e821ba7076be8f9
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-12 21:09:52 -0500

    updated

--- initialize_webserver_aws_ami.sh
+++ initialize_webserver_aws_ami.sh
@@ -19,7 +19,7 @@ echo "version: '3'" > docker-compose.yml
 echo "services:" >> docker-compose.yml
 echo "  jenkins:" >> docker-compose.yml
 #echo "    image: franklinjff/jenkins:version1" >> docker-compose.yml
-echo "    image: jharris10/jenkins:3" >> docker-compose.yml
+echo "    image: jharris10/jenkins:5" >> docker-compose.yml
 echo "    environment:" >> docker-compose.yml
 echo "      JAVA_OPTS: \"-Djava.awt.headless=true\"" >> docker-compose.yml
 echo "      JAVA_OPTS: \"-Djenkins.install.runSetupWizard=false\"" >> docker-compose.yml
