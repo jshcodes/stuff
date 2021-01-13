7b6e88bea18b8c84668ce8d30b857c1bb643784f
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-01-13 01:41:03 -0500

    Moved back to sh

--- start-exercise.sh
+++ start-exercise.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec -it $(docker container ls | grep 'attacker' | awk '{print $1}') /bin/bash
+echo "Welcome to the CrowdStrike attack simulation!"
+docker exec -it $(docker container ls | grep 'attacker' | awk '{print $1}') /bin/sh
