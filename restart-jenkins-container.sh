f51c85f5cfde3a66469bd03ebdb5569991974251
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-02-10 18:19:22 -0500

    Shiny and new

---
+++ restart-jenkins-container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Quick and dirty
+
+JID=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --filter "Name=instance-state-name,Values=running" --filter "Name=tag:demo-purp$
+echo "Jenkins server ID: $JID"
+CMD="cd /var/tmp;docker-compose down;docker-compose up -d"
+/usr/local/bin/exec-cmd -i $JID -c "$CMD" -w 10
