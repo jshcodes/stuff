02f1695cdb38aef459114cc2d68959c9384ce4f9
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-03-28 23:56:58 -0400

    Wirin' it togetha

---
+++ cspm-benchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if type aws >/dev/null 2>&1; then
+  echo "This is AWS"
+  curl -o aws_count.py https://raw.githubusercontent.com/jshcodes/stuff/main/aws-cspm-benchmark.py
+  python3 -m pip install boto3 tabulate
+  python3 aws_count.py
+  rm aws_count.py
+fi
+
+if type az >/dev/null 2>&1; then
+  echo "This is Azure"
+  curl -o azure_count.py https://raw.githubusercontent.com/jshcodes/stuff/main/azure-cspm-benchmark.py
+  python3 -m pip install azure-mgmt-resource azure-identity
+  python3 azure_count.py
+  rm azure_count.py
+fi
