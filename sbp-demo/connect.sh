974364d0d0608b1b31d3b5248d683f89226abdd8
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-05-06 19:50:41 -0400

    Fixed

--- sbp-demo/connect.sh
+++ sbp-demo/connect.sh
@@ -13,10 +13,10 @@ then
     echo "Available targets: linux1, linux2, windows1, windows2"
 elif [[ "$ACTION" == "linux1" ]]
 then
-    ssh -i $SSHKEY -o StrictHostKeyChecking=no $LINUX_1 $2 $3 $4
+    ssh -i $SSHKEY -o StrictHostKeyChecking=no ec2-user@$LINUX_1 $2 $3 $4
 elif [[ "$ACTION" == "linux2" ]]
 then
-    ssh -i $SSHKEY -o StrictHostKeyChecking=no $LINUX_2 $2 $3 $4
+    ssh -i $SSHKEY -o StrictHostKeyChecking=no ec2-user@$LINUX_2 $2 $3 $4
 elif [[ "$ACTION" == "win1" ]]
 then
     aws ssm start-session --target $WINDOWS_1
