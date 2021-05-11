bc180fbf62c8ce32e95aca34e0f1861fa9d0eb0b
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-05-11 12:26:01 -0400

    Fixed

--- sbp-demo/uninstall-agent.sh
+++ sbp-demo/uninstall-agent.sh
@@ -1,7 +1,7 @@
 payload='[{"Key":"InstanceIds","Values":["'$INSTANCE_ID'"]}]'
 result=$(aws ssm send-command --document-name "AWS-ConfigureAWSPackage" \
 	--document-version "1" --targets $payload \
-	--parameters '{"action":["Uninstall"],"installationType":["Uninstall and reinstall"],"name":["demo_JoshFalconSensor"],"version":[""],"additionalArguments":["{}"]}' \
+	--parameters '{"action":["Uninstall"],"installationType":["Uninstall and reinstall"],"name":["'$PACKAGE_NAME'"],"version":[""],"additionalArguments":["{}"]}' \
 	--timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region $REGION | jq .[].ErrorCount)
 if [[ "$result" == 0 ]]
 then
